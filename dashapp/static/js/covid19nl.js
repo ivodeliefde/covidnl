67ea03627cb30b518a205d6d6667516fa3e946a6
Ivo de Liefde <ivodeliefde@gmail.com>
2021-01-11 16:44:07 +0100

    Run javascript once page has been loaded or resized.

--- dashapp/static/js/covid19nl.js
+++ dashapp/static/js/covid19nl.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
+
+adjustLayout = function(){
     if ($(window).width() < 1200){
+        console.log("Optimize layout for small screen")
         $('#button_container').addClass('row').removeClass('col');
     } else {
+        console.log("Optimize layout for large screen")
         $('#button_container').addClass('col').removeClass('row');
     }
-})
+};
+
+$(document).ready(adjustLayout);
+window.addEventListener('load', adjustLayout);
+window.addEventListener('resize', adjustLayout);
+adjustLayout();
